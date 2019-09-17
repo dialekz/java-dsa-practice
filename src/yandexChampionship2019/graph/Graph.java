8bdee93d3d5841b91bbb4cfbbd4cdc5716564b37
dialekz <dialekz@gmail.com>
2019-09-17 11:19:14 +0300

    Update gradle to 5.6.2

--- src/yandexChampionship2019/graph/Graph.java
+++
@@ -1,6 +0,0 @@
-package yandexChampionship2019.graph;
-
-public interface Graph {
-
-    boolean thereIsWayBetweenNodes(GraphListImpl.Node a, GraphListImpl.Node b);
-}
--- src/yandexChampionship2019/graph/GraphMapImpl.java
+++
@@ -1,9 +0,0 @@
-package yandexChampionship2019.graph;
-
-public class GraphMapImpl implements Graph {
-
-    @Override
-    public boolean thereIsWayBetweenNodes(GraphListImpl.Node a, GraphListImpl.Node b) {
-        return false;
-    }
-}
