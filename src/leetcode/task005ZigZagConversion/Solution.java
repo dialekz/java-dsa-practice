436218207922e10e62aa2f461fc2095b677f5cb7
dialekz <dialekz@gmail.com>
2019-10-09 12:44:32 +0300

    Added LeetCode ZigZag Conversion solution

---
+++ src/leetcode/task005ZigZagConversion/Solution.java
@@ -0,0 +1,42 @@
+package leetcode.task005ZigZagConversion;
+
+public class Solution {
+    public String convert(String s, int numRows) {
+        if (s == null || numRows == 0) {
+            return "";
+        }
+
+        StringBuilder result = new StringBuilder(s.length());
+        char[] arr = s.toCharArray();
+        int initStep = (numRows - 1) * 2;
+        initStep = initStep > 0 ? initStep : 1;
+
+        for (int i = 0; i < numRows; i++) {
+            int stepOne = initStep - i * 2;
+            int stepTwo = initStep - stepOne;
+            boolean isFirst = true;
+            for (int j = i; j < arr.length; ) {
+                result.append(arr[j]);
+                if (stepOne == 0) {
+                    j += stepTwo;
+                } else if (stepTwo == 0) {
+                    j += stepOne;
+                } else {
+                    j += isFirst ? stepOne : stepTwo;
+                }
+                isFirst = !isFirst;
+            }
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.convert("PAYPALISHIRING", 3).equals("PAHNAPLSIIGYIR"));
+        System.out.println(solution.convert("PAYPALISHIRING", 4).equals("PINALSIGYAHRPI"));
+        System.out.println(solution.convert("abababab", 2).equals("aaaabbbb"));
+        System.out.println(solution.convert("abababab", 1).equals("abababab"));
+    }
+}
