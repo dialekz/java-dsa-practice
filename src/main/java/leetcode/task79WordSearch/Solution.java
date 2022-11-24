e6e47344bd58dc2ce94040d591867fc4a35814f1
dialekz <dialekz@gmail.com>
2022-11-24 22:31:39 +0400

    LeetCode 79. Word Search

--- src/main/java/leetcode/task79WordSearch/Solution.java
+++ src/main/java/leetcode/task79WordSearch/Solution.java
@@ -1,8 +1,5 @@
 package leetcode.task79WordSearch;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 /**
  * Given an m x n grid of characters board and a string word, return true if word exists in the grid.
  * <p>
@@ -12,36 +9,29 @@ import java.util.Queue;
  */
 class Solution {
 
+  byte[][] visited;
+  int[][] steps = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
   public boolean exist(char[][] board, String word) {
-    Queue<int[]> onStartQueue = new LinkedList<>();
-    findFirst(board, word.toCharArray()[0], onStartQueue);
+    visited = new byte[board.length][board[0].length];
+    boolean result;
 
-    byte[][] visited = new byte[board.length][board[0].length];
-    int[] start;
 
-    boolean result = word.length() == 1 && !onStartQueue.isEmpty();
-
-    while (!onStartQueue.isEmpty()) {
-      start = onStartQueue.poll();
-      visited[start[0]][start[1]] = 1;
-      result = result || findNext(board, 1, word, start[0], start[1], visited);
-      visited[start[0]][start[1]] = 0;
-    }
-    return result;
-  }
-
-  private void findFirst(char[][] board, char c, Queue<int[]> queue) {
-    int visitorNum = 1;
     for (int i = 0; i < board.length; i++) {
       for (int j = 0; j < board[i].length; j++) {
-        if (board[i][j] == c)
-          queue.add(new int[]{i, j, 0, visitorNum++});
+        if (board[i][j] == word.charAt(0)) {
+          visited[i][j] = 1;
+          result = word.length() == 1 || findNext(board, 1, word, i, j);
+          if (result)
+            return true;
+          visited[i][j] = 0;
+        }
       }
     }
+    return false;
   }
 
-  private boolean findNext(char[][] board, int current, String word, int y, int x, byte[][] visited) {
-    int[][] steps = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+  private boolean findNext(char[][] board, int current, String word, int y, int x) {
     int[] variant;
     boolean result = false;
     for (int[] step : steps) {
@@ -57,7 +47,7 @@ class Solution {
           return true;
         else {
           visited[variant[0]][variant[1]] = 1;
-          result = result || findNext(board, current + 1, word, variant[0], variant[1], visited);
+          result = result || findNext(board, current + 1, word, variant[0], variant[1]);
           visited[variant[0]][variant[1]] = 0;
         }
       }
