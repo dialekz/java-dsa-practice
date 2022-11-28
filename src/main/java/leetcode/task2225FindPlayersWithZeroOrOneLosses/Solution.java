f19ba742205ee637fd732cba40d67dab245a0401
dialekz <dialekz@gmail.com>
2022-11-28 23:14:01 +0400

    LeetCode 2225. Find Players With Zero or One Losses

--- src/main/java/leetcode/task2225FindPlayersWithZeroOrOneLosses/Solution.java
+++ src/main/java/leetcode/task2225FindPlayersWithZeroOrOneLosses/Solution.java
@@ -1,12 +1,7 @@
 package leetcode.task2225FindPlayersWithZeroOrOneLosses;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 /**
  * You are given an integer array matches where matches[i] = [winner[i], loser[i]]
@@ -25,28 +20,33 @@ import java.util.stream.Collectors;
 class Solution {
 
   public List<List<Integer>> findWinners(int[][] matches) {
-    HashMap<Integer, Integer> losers = new HashMap<>(matches.length);
-    HashSet<Integer> winners = new HashSet<>(matches.length);
-    Integer winner, loser, loses;
-
+    int players = 0;
     for (int[] game : matches) {
-      loser = game[1];
-      loses = Optional.ofNullable(losers.get(loser)).orElse(0);
-      if (loses < 2)
-        losers.put(loser, loses + 1);
+      players = Integer.max(players, game[0]);
+      players = Integer.max(players, game[1]);
     }
 
+    int[] score = new int[players + 1];
     for (int[] game : matches) {
-      winner = game[0];
-      if (!losers.containsKey(winner))
-        winners.add(winner);
+      if (score[game[0]] == 0)
+        score[game[0]] = 1;
 
+      if (score[game[1]] == 0)
+        score[game[1]] = 1;
+
+      score[game[1]] += 1;
     }
 
-    return List.of(
-        new ArrayList<>(winners),
-        new ArrayList<>(losers.entrySet().stream()
-            .filter(entry -> entry.getValue() == 1)
-            .map(Map.Entry::getKey).collect(Collectors.toList())));
+    List<Integer> winners = new LinkedList<>();
+    List<Integer> losers = new LinkedList<>();
+
+    for (int i = 0; i < score.length; i++) {
+      if (score[i] == 1)
+        winners.add(i);
+      else if (score[i] == 2)
+        losers.add(i);
+    }
+
+    return List.of(winners, losers);
   }
 }
